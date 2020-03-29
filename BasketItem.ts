ce902ca8b04748098ddfd93afcdc343c9ad1a548
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2020-03-29 18:47:24 +0000

    axios dep, parse xml

---
+++ BasketItem.ts
@@ -0,0 +1,10 @@
+
+export interface BasketItem {
+    timestamp: Date;
+    value: number;
+}
+
+export interface ListItem {
+    '@_data': string;
+    '@_val': string;    
+}
--- server.ts
+++ server.ts
@@ -1,11 +1,29 @@
 
-import express, { json } from 'express';
 import { config } from 'dotenv'
-import { router as basketRouter } from './routes/basket';
+import axios, { AxiosResponse} from 'axios';
+import { parse } from 'fast-xml-parser';
+import { BasketItem, ListItem } from './BasketItem';
 
 config();
+const { BASKET_URL } = process.env;
 
-const app: express.Application = express();
-app.use(json());
-app.use('/basket', basketRouter);
-app.listen(3000, () => console.log('server started'));
+if (!BASKET_URL) {
+    throw new TypeError(`Valid basket URL must be configured, found => "${BASKET_URL}"`);
+}
+
+const normalizer = (list: ListItem[]): BasketItem[] => {
+    return list.map((item: ListItem): BasketItem => {
+        const { '@_data': data, '@_val': val } = item;
+        return { timestamp: new Date(data), value: parseFloat(val)};
+    });
+};
+
+const basket: Promise<BasketItem[]> = axios.get(BASKET_URL)
+    .then(({ data }: AxiosResponse<string>): BasketItem[] => {
+        const { Basket: { BasketList: list}} = parse(data, {ignoreAttributes: false});
+        console.log(`basket length => ${list.length}`);
+        return normalizer(list);
+    })
+;
+
+basket.then((basket: BasketItem[]) => console.log(basket));
